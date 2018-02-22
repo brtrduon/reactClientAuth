501e73006414864498a46226fd443f924bc809ff
unknown <you@example.com>
2018-02-22 14:36:37 -0800

    added browserHistory

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { browserHistory } from 'react-router';
 
 const ROOT_URL = 'http://localhost:8000';
 
@@ -11,14 +12,19 @@ export function signinUser({ email, password}) {
 
 
         // submit email/password to the server
-        axios.post(`${ROOT_URL}/signin`, { email, password });
+        axios.post(`${ROOT_URL}/signin`, { email, password })
+            .then(response => {
+                // if request is good:
+                // -update state to indicate user is authenticated
+                // -save the JWT token
+                // -redirect to the route '/feature'
+                browserHistory.push('/feature');
+            })
+            .catch(() => {
+                // if request is bad:
+                // -show an error to the user
+
+            });
     
-        // if request is good:
-        // -update state to indicate user is authenticated
-        // -save the JWT token
-        // -redirect to the route '/feature'
-    
-        // if request is bad:
-        // -show an error to the user
     }
 }
