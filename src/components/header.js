12c3a99d6d48d17cda7e992b811d64ac00a68bfd
unknown <you@example.com>
2018-02-23 18:00:22 -0800

    added signout component

--- src/components/header.js
+++ src/components/header.js
@@ -6,8 +6,7 @@ class Header extends Component {
     renderLinks() {
         if (this.props.authenticated){
             // show a link to sign out
-            return 
-            <li className='nav-item'>
+            return <li className='nav-item'>
                 <Link className='nav-link' to='/signout'>Sign Out</Link>
             </li>
         }
@@ -42,4 +41,4 @@ function mapStateToProps(state) {
     };
 }
 
-export default connect()(Header);
+export default connect(mapStateToProps)(Header);
