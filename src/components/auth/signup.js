e0081bfe520a1ff973cd3f2a336eb2514dbe6d62
unknown <you@example.com>
2018-02-23 18:11:00 -0800

    added signup component

---
+++ src/components/auth/signup.js
@@ -0,0 +1,13 @@
+import react, { Component } from 'react';
+import { reduxForm } from 'redux-form';
+import * as actions from '../../actions';
+
+class Signup extends Component {
+    render() {
+        return (
+            <div>Signup</div>
+        );
+    }
+}
+
+export default Signup;
