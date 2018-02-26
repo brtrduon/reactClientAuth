3b1cf9ddf9ac30d74ee5a2ea44c2e011e3189e70
unknown <you@example.com>
2018-02-26 09:52:53 -0800

    aded require_auth higher order component

---
+++ src/components/auth/require_auth.js
@@ -0,0 +1,35 @@
+// this is our higher order component to ensure that the user is logged in
+
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+export default function(ComposedComponent) {
+  class Authentication extends Component {
+    static contextTypes = {
+      router: React.PropTypes.object
+    }
+
+    componentWillMount() {
+      if (!this.props.authenticated) {
+        this.context.router.push('/');
+      }
+    }
+
+    componentWillUpdate(nextProps) {
+      if (!nextProps.authenticated) {
+        this.context.router.push('/');
+      }
+    }
+
+    render() {
+      return <ComposedComponent {...this.props} />
+    }
+  }
+
+  function mapStateToProps(state) {
+    return { authenticated: state.auth.authenticated };
+    // we need to put 'state.auth.authenticated' b/c we have an auth_reducer that produces the 'auth' piece of state
+  }
+
+  return connect(mapStateToProps)(Authentication);
+}
